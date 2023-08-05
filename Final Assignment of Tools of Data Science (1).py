5169a6dabe31bd3f8b2d247b241690ed6491785e
Rohit Dvv <91962692+rohitdvv@users.noreply.github.com>
2023-08-05 11:15:30 +0530

    Add files via upload

---
+++ Final Assignment of Tools of Data Science (1).py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Final Assingment
+
+# This is the final assignment of this module.Let's start this one.
+
+# ### Data Science Languages
+# 
+# Here are some of the most popular data science languages:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Java
+# 5. Julia
+# 6. Scala
+# 7. JavaScript
+# 8. Swift
+# 9. Go
+# 10. MATLAB
+# 
+# These languages are all powerful and versatile, and they can be used for a variety of data science tasks.
+
+# ### Popular Data Science Libraries
+# 
+# Here are some of the most popular data science libraries in Python:
+# 
+# 1. NumPy
+# 2. SciPy
+# 3. Pandas
+# 4. Matplotlib
+# 5. Scikit-Learn
+# 6. TensorFlow
+# 7. PyTorch
+# 8. Keras
+# 9. Seaborn
+# 10. Bokeh
+# 
+# These libraries provide a wide range of functionality for data science tasks, such as data manipulation, analysis, visualization, and machine learning.
+
+# ### Data Science Tools
+# 
+# | Tool | Programming Language |
+# |---|---|
+# | NumPy | Python | 
+# | SciPy | Python | 
+# | Pandas | Python |
+# | Matplotlib | Python |
+# | Scikit-Learn | Python | 
+# | TensorFlow | Python | 
+# | PyTorch | Python | 
+# | Keras | Python | 
+# | Seaborn | Python | 
+# | Bokeh | Python | 
+# 
+# This is just a small selection of the many data science tools available. There are many other great tools out there, so I encourage you to explore and find the ones that work best for you.
+# 
+
+# ### Arithmetic Expression Examples
+# 
+# An arithmetic expression is a combination of numbers, variables, and operators that represents a mathematical calculation. The operators in an arithmetic expression can be addition (+), subtraction (-), multiplication (*), division (/), and exponentiation (**).
+# 
+# Here are some examples of arithmetic expressions:
+# 
+# * 5 + 2
+# * 10 - 3
+# * 2 * 3
+# * 10 / 2
+# * 2 ** 3
+# 
+# These expressions can be evaluated to produce a numerical value. For example, the expression `5 + 2` evaluates to 7.
+# 
+# 
+
+# In[6]:
+
+
+a=5
+b=10
+c=a+b
+d=a*b
+print(c)
+print(d)
+print(c+d)
+
+
+# ### Convert Minutes to Hours
+
+# In[8]:
+
+
+min=120
+hours=min//60 
+print(hours)
+
+
+# ### Objectives
+# 
+# * To learn how to create markdown cells in Jupyter Notebooks.
+# * To learn how to use Markdown syntax to format text.
+# * To learn how to create lists, links, and images in Markdown.
+# * To learn how to use Markdown to create well-formatted and readable text.
+# 
+
+# # Rohit Dusanapudi
+
+# In[ ]:
+
+
+
+
